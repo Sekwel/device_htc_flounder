c60c65e77ea9655c95e9f0b920f279bbba384ccb
Mike Wielgosz <mike@sekwel.com>
2015-11-03 09:40:02 -0500

    Revert PRODUCT_NAME to aosp_flounder to match vendorsetup

--- aosp_flounder.mk
+++ aosp_flounder.mk
@@ -36,7 +36,7 @@ $(call inherit-product, device/htc/flounder/device-lte.mk)
 $(call inherit-product-if-exists, vendor/htc/flounder_lte/device-vendor.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
 
-PRODUCT_NAME := sekwel_flounder
+PRODUCT_NAME := aosp_flounder
 PRODUCT_DEVICE := flounder
 PRODUCT_BRAND := google
 PRODUCT_MODEL := Nexus 9
